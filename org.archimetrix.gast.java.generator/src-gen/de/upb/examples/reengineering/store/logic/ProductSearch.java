3273a81f3bc1364bed26891bb1321b9678bbb34a
Markus.vonDetten@gmail.com <Markus.vonDetten@gmail.com@ec899564-b4ac-3aba-93e1-dd24fd28e46c>
2012-07-19 12:04:17 +0000

    First prototype of Java code generation for GAST files with XPand.

---
+++ org.archimetrix.gast.java.generator/src-gen/de/upb/examples/reengineering/store/logic/ProductSearch.java
@@ -0,0 +1,28 @@
+package de.upb.examples.reengineering.store.logic;
+
+import java.lang.String;
+
+import java.lang.Object;
+
+import de.upb.examples.reengineering.store.logic.ISearch;
+
+import de.upb.examples.reengineering.store.logic.ProductSearch;
+
+public class ProductSearch
+
+implements
+
+ISearch
+
+{
+
+	public double searchPrice() {
+	}
+
+	public double searchDiscount() {
+	}
+
+	public String searchProducer() {
+	}
+
+}
---
+++ org.archimetrix.gast.java.generator/src-gen/de/upb/examples/reengineering/store/ui/IListView.java
@@ -0,0 +1,15 @@
+package de.upb.examples.reengineering.store.ui;
+
+import java.lang.String;
+
+import java.lang.Object;
+
+import de.upb.examples.reengineering.store.ui.IListView;
+
+public interface IListView
+
+{
+
+	public String createListEntry();
+
+}
