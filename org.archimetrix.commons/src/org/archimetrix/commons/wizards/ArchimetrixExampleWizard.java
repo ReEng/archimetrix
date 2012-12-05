1c5e73e686114171abb8545a6aae70bec634e60c
Markus.vonDetten@gmail.com <Markus.vonDetten@gmail.com@ec899564-b4ac-3aba-93e1-dd24fd28e46c>
2012-12-05 09:29:25 +0000

    Added icons to binary build properties

---
+++ org.archimetrix.commons/src/org/archimetrix/commons/wizards/ArchimetrixExampleWizard.java
@@ -0,0 +1,34 @@
+package org.archimetrix.commons.wizards;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+
+public class ArchimetrixExampleWizard extends Wizard implements INewWizard
+{
+
+   // TODO: Implement this wizard...
+   
+   public ArchimetrixExampleWizard()
+   {
+      // TODO Auto-generated constructor stub
+   }
+
+
+   @Override
+   public void init(IWorkbench workbench, IStructuredSelection selection)
+   {
+      // TODO Auto-generated method stub
+
+   }
+
+
+   @Override
+   public boolean performFinish()
+   {
+      // TODO Auto-generated method stub
+      return false;
+   }
+
+}
