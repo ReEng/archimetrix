13f9cd82bb271c257da83c6461c2ccb328b9fb41
Markus.vonDetten@gmail.com <Markus.vonDetten@gmail.com@ec899564-b4ac-3aba-93e1-dd24fd28e46c>
2013-01-30 15:06:03 +0000

    Created branch for Juno development.

---
+++ org.archimetrix.architecturepreview.ui/src/org/archimetrix/architectureprognosis/ui/ArchitecturePrognosisUIPlugin.java
@@ -0,0 +1,62 @@
+package org.archimetrix.architectureprognosis.ui;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class ArchitecturePrognosisUIPlugin extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.archimetrix.architecturepreview.ui"; //$NON-NLS-1$
+
+	// The shared instance
+   private static ArchitecturePrognosisUIPlugin plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public ArchitecturePrognosisUIPlugin() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(final BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(final BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static ArchitecturePrognosisUIPlugin getDefault() {
+		return plugin;
+	}
+	
+	  /**
+    * Returns an image descriptor for the image file at the given
+    * plug-in relative path
+    *
+    * @param path the path
+    * @return the image descriptor
+    */
+   public static ImageDescriptor getImageDescriptor(final String path) {
+      return imageDescriptorFromPlugin(PLUGIN_ID, path);
+   }
+
+}
