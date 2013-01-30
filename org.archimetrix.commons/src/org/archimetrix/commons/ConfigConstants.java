80e544b556f9386c667229fa9e63a6e22b9332f0
Markus.vonDetten@gmail.com <Markus.vonDetten@gmail.com@ec899564-b4ac-3aba-93e1-dd24fd28e46c>
2013-01-30 15:06:45 +0000

    Created branch for Juno development.

---
+++ org.archimetrix.commons/src/org/archimetrix/commons/ConfigConstants.java
@@ -0,0 +1,24 @@
+package org.archimetrix.commons;
+
+
+/**
+ * Provides constants for reading the config files.
+ * 
+ * @author mcp
+ * @author Last editor: $Author$
+ * @version $Revision$ $Date$
+ * 
+ */
+public interface ConfigConstants
+{
+   public static final String CONFIG_PROPERTIES_FILE = "/config/config.properties";
+
+   public static final String PROPERTY_SEPARATOR = ";";
+
+   
+   public static final String RELEVANCEANALYSIS_EPSILON = "relevanceanalysis.epsilon";
+
+   public static final String RELEVANCEANALYSIS_ANNOTATION_NAMES = "relevanceanalysis.available_annotation_names";
+
+
+}
---
+++ org.archimetrix.commons/src/org/archimetrix/commons/handlers/OpenLaunchDialogHandler.java
@@ -0,0 +1,25 @@
+package org.archimetrix.commons.handlers;
+
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.debug.ui.DebugUITools;
+import org.eclipse.debug.ui.IDebugUIConstants;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+
+public class OpenLaunchDialogHandler extends AbstractHandler
+{
+
+   @Override
+   public Object execute(ExecutionEvent event) throws ExecutionException
+   {
+      DebugUITools.openLaunchConfigurationDialogOnGroup(
+            HandlerUtil.getActiveShell(event), new StructuredSelection(),
+            IDebugUIConstants.ID_RUN_LAUNCH_GROUP);
+      return null;
+   }
+
+}
