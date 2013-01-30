f0d7766dc4727eb1cca8aee4dd699613a67cf3d8
Markus.vonDetten@gmail.com <Markus.vonDetten@gmail.com@ec899564-b4ac-3aba-93e1-dd24fd28e46c>
2013-01-30 15:11:20 +0000

    Created branch for Juno development.

---
+++ org.reclipse.structure.inference.extended.ui/src/org/reclipse/structure/inference/extended/ui/FilteringAdapterFactoryContentProvider.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package org.reclipse.structure.inference.extended.ui;
+
+import java.util.ArrayList;
+
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.edit.ui.provider.AdapterFactoryContentProvider;
+
+import eu.qimpress.samm.staticstructure.ComponentType;
+
+/**
+ * @author Oleg
+ * @author Last editor: $Author$
+ * @version $Revision$ $Date$
+ *
+ */
+public class FilteringAdapterFactoryContentProvider extends AdapterFactoryContentProvider
+{
+
+   private final static String DUMMY = "SoMoX System-Level Dummy Component";
+   public FilteringAdapterFactoryContentProvider(AdapterFactory adapterFactory)
+   {
+      super(adapterFactory);
+   }
+
+   @Override
+   public Object[] getElements(Object object)
+   {
+      Object[] result = super.getElements(object);
+      ArrayList<Object> list = new ArrayList<Object>();
+      if(result != null)
+      {
+         for(Object o : result)
+         {
+            if(o instanceof ComponentType)
+            {
+               if(!DUMMY.equals(((ComponentType) o).getName()))
+               {
+                  list.add(o);
+               }
+            }
+         }
+      }
+      return list.toArray();
+   }
+
+   @Override
+   public Object[] getChildren(Object object)
+   {
+//      return super.getChildren(object);
+      return null;
+   }
+
+   @Override
+   public boolean hasChildren(Object object)
+   {
+//      return super.hasChildren(object);
+      return false;
+   }
+
+}
---
+++ org.reclipse.structure.inference.extended.ui/src/org/reclipse/structure/inference/extended/ui/StartModifyingCatalogWizard.java
@@ -0,0 +1,108 @@
+/**
+ * 
+ */
+package org.reclipse.structure.inference.extended.ui;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.jface.wizard.IWizardPage;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.PlatformUI;
+import org.reclipse.structure.inference.InferenceEngine;
+import org.reclipse.structure.inference.InterpreterInferenceEngine;
+import org.reclipse.structure.inference.extended.ModifyCatalogAndGenerateAlgorithmsAction;
+import org.reclipse.structure.inference.ui.wizards.StartInferenceWizard;
+
+/**
+ * @author Oleg
+ * @author Last editor: $Author$
+ * @version $Revision$ $Date$
+ *
+ */
+public class StartModifyingCatalogWizard extends StartInferenceWizard
+{
+   private ComponentSelectionCatalog selectionPage;
+   
+   public StartModifyingCatalogWizard(IWorkbench workbench)
+   {
+      super();
+   }
+
+   
+   @Override
+   public void addPages()
+   {
+      this.page = new StartModifyingCatalogWizardPage("Design Deficiency Detection");
+      //this resource set will only be used for selection and generating story diagrams
+      //on perform finish call it will be reseted again
+      this.page.initializeResourceSet();
+      this.addPage(this.page);
+      //add second page here
+      this.selectionPage = new ComponentSelectionCatalog("Select Components to be analyzed");
+      this.addPage(selectionPage);
+      
+   }
+   
+   
+   @Override
+   public IWizardPage getNextPage(IWizardPage page)
+   {
+      IWizardPage next = super.getNextPage(page);
+      this.selectionPage.setSelection(this.page.getRoot());
+      return next;
+   }
+
+
+   /**
+    * @see org.eclipse.jface.wizard.Wizard#performFinish()
+    */
+   @Override
+   public boolean performFinish()
+   {
+      return super.performFinish();
+   }
+
+   @Override
+   protected void storePageSettings()
+   {
+      super.storePageSettings();
+   }
+
+   @Override
+   protected InferenceEngine getInferenceEngine()
+   {
+      // generate here
+      Object[] selection = null;
+      if (!selectionPage.isAllChecked())
+      {
+         selection = selectionPage.getCheckedObjects();
+      }
+      Resource catalog = page.getCatalog();
+      Resource engines = page.getEngines();
+      if (!this.page.isUseExistingEngines())
+      {
+         URI uri = URI.createPlatformResourceURI(catalog.getURI().toPlatformString(false) + ".ecore", true);
+         engines = catalog.getResourceSet().createResource(uri);
+      }
+
+      ModifyCatalogAndGenerateAlgorithmsAction generateAction = new ModifyCatalogAndGenerateAlgorithmsAction(catalog,
+            engines, selection);
+      // configure view
+      PlatformUI.getWorkbench().getDisplay().syncExec(generateAction);
+
+      // prepare engine
+      String name = "Archimetrix Deficiency Detection";
+      final InferenceEngine engine = new InterpreterInferenceEngine(name);
+
+      engine.setAstRoot(page.getRoot());
+      engine.setCatalog(page.getCatalog());
+      engine.setEngines(page.getEngines());
+      engine.setAnnotationEvaluator(page.getEvaluator());
+      engine.setSearchForAdditionalElements(page.isSearchForAdditionals());
+      engine.setReportLevel(page.getReportLevel());
+      return engine;
+   }
+
+
+
+}
